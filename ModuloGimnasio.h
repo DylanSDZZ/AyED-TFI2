27ef3c317f1b7ac5647d240bb911c311571995d0
Fer <fernandojimenez064@gmail.com>
2023-02-02 06:42:59 -0300

    Modulo Entrenador: Comentarios

--- ModuloGimnasio.h
+++ ModuloGimnasio.h
@@ -5,40 +5,41 @@
 void menuIngreso(int legajo);
 
 void inicioSesion(){
-	system("cls");
-	FILE *archivo;
-	entrenador aux;
-	int b,legajo;
 	
-	char USER[20],CLAVE[40];
+	system("cls"); 
+	FILE *archivo;   // DECLARA UN ARCHVO
+	entrenador aux;   // DECLARA LOS STRUCTS CORRESPONDIENTES A LOS ENTRENADORES
+	int b,legajo; 
 	
-	archivo=fopen("PruebaEntrenadores.dat","rb");
+	char USER[20],CLAVE[40];   // VARIABLES DONDE SE GUARDARAN LOS DATOS INGRESADOS PARA EL INICIO DE SESION
+	
+	archivo=fopen("PruebaEntrenadores.dat","rb");   // ABRE EL ARCHIVO DONDE SE GUARDAN LOS DATOS DE LOS USUARIOS DE ENTRENADORES
 	b=0;
 	
 	printf("\n\n---------------------%c INICIO DE SESION - ENTRENADORES %c---------------------\n", 04, 04);
 	
 	printf("\n %c Ingrese El nombre de usuario: ",04);
 	_flushall();
-	gets(USER);
+	gets(USER);   // CONSIGUE EL NICK DEL USUARIO QUE DESEA INICIAR SESION
 	printf("\n %c Ingrese la contrase%ca: ",04,164);
 	_flushall();
-	gets(CLAVE);
+	gets(CLAVE);  // CONSIGUE LA CONTRA DEL USUARIO QUE DESEA INICIAR SESION
 	
 	
-	rewind(archivo);
-	fread(&aux,sizeof(aux),1,archivo);
+	rewind(archivo);        // COLOCA EL PUNTERO AL INICIO DEL ARCHIVO PARA QUE PUEDA COMPARAR CADA UNO DE LOS DATOS DENTRO DEL ARCHIVO
+	fread(&aux,sizeof(aux),1,archivo); // REALIZA LA PRIMERA LECTURA DEL ARCHIVO 
 	
-	while(!feof(archivo)){
+	while(!feof(archivo)){  
 		if(strcmp(USER,aux.nick) == 0 and strcmp(CLAVE,aux.pass)== 0){
-			legajo=aux.legajo; // Crea una variable a la cual se le asigna el legajo del entrenador que inicio sesion.
-			menuIngreso(legajo); // Llama a la funcion MENU mandando como dato el valor del legajo
-			b=1;
+			legajo=aux.legajo; // CREA UNA VARIABLE A LA CUAL SE LE ASIGNA EL NUMERO DE LEGAJO DEL ENTRENADOR QUE INICIO SESION
+			menuIngreso(legajo); // LLAMA LA FUNCION DE MENU DEL ENTRENADOR LLEVANDO EL NUMERO DE LEGAJO DEL MISMO
+			b=1;   // COLOCA LA BANDERA EN 1 PARA QUE EL CICLO SE ROMPA
 		}else{
-			fread(&aux,sizeof(aux),1,archivo);
+			fread(&aux,sizeof(aux),1,archivo);  // SI NO ENCONTRO CORRESPONDENCIA ENTRE LOS DATOS INGRESADO EN LA PRIMERA LINEA DEL ARCHIVO, SIGUE BUSCANDO EN LAS PROXIMAS LINEAS
 		}
 	}
 	
-	if(b==0){
+	if(b==0){   // SI NUNCA ENCONTRO LA COINCIDENCIA DE DATOS DENTRO DEL ARCHIVO, LOS DATOS INGRESADOS FUERON INCORRECTOS
 	   printf("\n\t\tUSUARIO O CLAVE INCORRECTOS.");
        printf("\n\n");
        system("pause");
@@ -47,62 +48,63 @@ void inicioSesion(){
 	
 }
 
-int validarCod(int codigo){
-	rutina v;
-	FILE *arch;
-
-	arch = fopen("rutinas.dat","rb");
+int validarCod(int codigo){  // FUNCION QUE VALIDA EL CODIGO DE RUTINA 
+	rutina v;   // DECLARA EL TIPO DE DATO ESTRUCTURADO RUTINA
+	FILE *arch; // DECLARA UN ARCHIVO CON EL CUAL VAMOS  A TRABAJAR
+ 
+	arch = fopen("rutinas.dat","rb");  // ABRE EL ARCHIVO EN MODO LECTURA
 
 	
-	fread(&v,sizeof(v),1,arch);
+	fread(&v,sizeof(v),1,arch);  // REALIZA LA PRIMERA LECTURA DEL CICLO
 	
-	while(feof(arch)){
+	while(feof(arch)){   // SI EL ARCHIVO ESTA VACIO RETORNA UN VALOR QUE PERMITE INGRESAR EL CODIGO
 		return 1;
 	}
 	
-	while(!feof(arch)){
+	while(!feof(arch)){   // SI EL ARCHIVO NO ESTA VACIO COMIENZA A COMPARAR LOS DATOS
 		if(v.CodRut==codigo){  // COMPRUEBA SI EL CODIGO GENERADO YA FUE OCUPADO EN LE ARCHIVO
-			fclose(arch);
-			return 0;
+			fclose(arch);     // SI SE ENCONTRO EL MISMO CODIGO YA REGISTRADO CIERRA EL ARCHIVO 
+			return 0;     // RETORNA UN 0 EN SEÑAL DE QUE NO SE PUDO REGISTRAR EL CODIGO YA QUE ESTABA OCUPADO PREVIAMENTE
 		}else{
-			fread(&v,sizeof(v),1,arch);
+			fread(&v,sizeof(v),1,arch); // REALIZA LA SEGUNDA LECTURA DEL ARCHIVO
 		}
-		fclose(arch);
-		return 1;
+		
+		fclose(arch); 
+		return 1; // RETORNA 1 EN SEÑAL DE QUE EL CODIGO NO FUE OCUPADO PREVIAMENTE
 	}
 	
 }
 
-int genCod(int num){
+int genCod(int num){   // FUNCION QUE GENERA EL CODIGO DE RUTINA ALEATORIAMENTE
 	int i,x;
-	srand(time(NULL));
+	srand(time(NULL)); 
 	
 	do{
-		for(i=0;i<1;i++){
-			x = 1+rand()%100;
+		for(i=0;i<1;i++){    
+			x = 1+rand()%100;  // ASIGNA A LA VARIABLE UN NUMERO ENTRE EL 1 Y EL 100
 		}
-	}while(!(validarCod(x)));
-	return x;
+	}while(!(validarCod(x)));  // COMPRUEBA QUE EL CODIGO GENERADO NO HAYA SIDO OCUPADO PREVIAMENTE 
+	return x; // RETORNA EL VALOR DE LA VARIABLE X CON EL CODIGO GENERADO
 }
 
 
 
-int cantidadPalabras(char *frase){
-	char caracterActual;
+int cantidadPalabras(char *frase){  // FUNCION QUE CUENTA LAS PALABRAS DENTRO DEL ARRAY DE RUTINAS
+	char caracterActual;  
 	char caracterPrevio = ' ';
 	int cont=0;
 	
-	while(*frase != '\0'){
-		caracterActual = *frase;
+	while(*frase != '\0'){   /
+		caracterActual = *frase; 
 		
-		if(caracterActual == ' ' and caracterPrevio != ' '){
+		if(caracterActual == ' ' and caracterPrevio != ' '){ 
 			
-			cont++;
+			cont++;  
 		}
 		
-		caracterPrevio = caracterActual;
+		caracterPrevio = caracterActual;  
 		
-		frase++;
+		frase++; 
 		
 	}
 	
@@ -110,45 +112,47 @@ int cantidadPalabras(char *frase){
 		cont++;
 	}
 	
-	return cont;
+	return cont; // RETORNA LA CANTIDAD DE PALABRAS 
 }
 
-void registrarRutinas(int legajo){
+void registrarRutinas(int legajo){  // FUNCION QUE REGISTRA LAS RUTINAS 
 	system("cls");
-	rutina v;
-	FILE *rutinas;
-	b=0;
+	rutina v; // DECLARA LA ESTRUCTURA DE RUTINAS
+	FILE *rutinas; // DECLARA EL ARCHIVO QUE VAMOS A USAR
+	b=0; // COLOCA LA BANDERA EN 0
 	char res;
 	char resp;
 	int cantpalabras;
 	
-	rutinas=fopen("rutinas.dat","ab");
+	rutinas=fopen("rutinas.dat","ab");  // ABRE EL ARCHIVO 
 	
 	do{
 		printf("\n\n--------------------------%c REGISTRO DE RUTINAS %c--------------------------\n", 04, 04);
 		_flushall();
-		v.CodRut=genCod(v.CodRut); // Aca le asigno el valor random generado a la variable CodRut
+		v.CodRut=genCod(v.CodRut); // SE LE ASIGNA UN VALOR GENERADO CON LA FUNCION A EL CODIGO DE RUTINA
 		
-		printf("\n\n\t\t\tCodigo de Rutina:%d",v.CodRut);
+		printf("\n\n\t\t\tCodigo de Rutina:%d",v.CodRut);  // LO IMPRIME EN PANTALLA
 		
 		do{
 		
-		 printf("\n\t\t\tDescripcion de Rutina: ");              
+		 printf("\n\t\t\tDescripcion de Rutina: ");                  
 		 gets(v.Descripcion);
-	     cantpalabras=cantidadPalabras(v.Descripcion); // Llama a la funcion que va a contar la cantidad de palabras mandando como dato el string guardado en la variable
+	     cantpalabras=cantidadPalabras(v.Descripcion); // LLAMA LA FUNCION QUE CUENTA LAS PALABRAS Y LE ASIGNA EL VALOR A LA VARIABLE CANTPALABRAS
          
-		 if(cantpalabras>360){
+		 if(cantpalabras>360){   
+		 	
          	printf("\n La rutina ingresada supera el limite de contenido, intente nuevamente . . .");
 		 }
-	   }while(cantpalabras>360);
+	    }while(cantpalabras>360); // SI LA CANTIDAD DE PALABRAS ES MAYOR A 360 VUELVE A REPETIR EL CICLO YA QUE SUPERA EL LIMITE PERMITIDO
         
 
 	    
 		
-		fwrite(&v,sizeof(v),1,rutinas);
+		fwrite(&v,sizeof(v),1,rutinas); // UNA VEZ REGISTRADA LA RUTINA LA ESCRIBE EN EL ARCHIVO 
 		
 		
-		
+		printf("\n\t\t\tRutina Registrada con exito... Legajo de Entrenador a cargo: %d",legajo); // IMPRIME EL EXITO AL REGISTRAR LA RUTINA JUNTO AL LEGAJO DEL ENTRENADOR QUE LA INGRESO
+		printf("\n\t\t\t");system("pause");
 		printf("\n\n\t\t\tDesea registrar una nueva rutina? [S/N]: ");
 		_flushall();
 		scanf("%c",&res);
@@ -159,7 +163,6 @@ void registrarRutinas(int legajo){
 			b=1;
 		}
 		
-		
 	}while(b==0);
 	
 	fclose(rutinas);
@@ -167,7 +170,7 @@ void registrarRutinas(int legajo){
 	
 }
 
-void menuIngreso(int legajo){
+void menuIngreso(int legajo){   // RECIBE EL NUMERO DE LEGAJO DEL ENTRENADOR QUE INICIO SESION
 	entrenador aux;
 	system("cls");
 	int op;
@@ -185,11 +188,11 @@ void menuIngreso(int legajo){
 		switch(op){
 
 			case 1:
-				//listarSocios(legajo); // Envia el legajo del entrenador a la funcion listarSocios
+				//listarSocios(legajo); // ENVIA A LA FUNCION LISTAR SOCIOS EL NUMERO DE LEGAJO GUARDADO EN LA VARIABLE LEGAJO  // FUNCION TODAVIA NO CREADA
 				break;
 
 			case 2:
-				  registrarRutinas(legajo); // Envia el valor de la variable legajo a la funcion registrar rutina
+				  registrarRutinas(legajo); // ENVIA EL NUMERO DE LEGAJO DEL ENTRENADOR A LA FUNCION REGISTRAR RUTINA 
 				break;
 			case 0: 
 			     system("cls");
